b1e63fae8c1df5db060883e4a915721a3119dd01
Marialopez2020 <lopezmariasoledad2020@gmail.com>
2024-04-21 18:59:49 -0300

    Adding new post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,15 @@
-import Post from './Post';
-import styles from './PostList.module.css';
+import Post from "./Post";
+import NewPost from "./NewPost";
+import styles from "./PostList.module.css";
 function PostList() {
   return (
-    <ul className={styles.posts}>
-      <Post author="Maria" body="React.js is Awesome!" />
-      <Post author="Missie" body="Java is Awesome!" />
-    </ul>
+    <>
+     <NewPost />
+      <ul className={styles.posts}>
+        <Post author="Maria" body="React.js is Awesome!" />
+        <Post author="Missie" body="Java is Awesome!" />
+      </ul>
+    </>
   );
 }
 
