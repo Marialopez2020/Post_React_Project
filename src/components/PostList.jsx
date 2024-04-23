ac2dda71d9c82bcbbc2c1ab57873b642e74c2de3
Marialopez2020 <lopezmariasoledad2020@gmail.com>
2024-04-23 07:51:51 -0300

    Adding modal

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,23 +1,38 @@
 import { useState } from "react";
 import Post from "./Post";
+import Modal from "./Modal";
 import NewPost from "./NewPost";
 import styles from "./PostList.module.css";
+
 function PostList() {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
-function bodyChangeHandler(event){
-  setEnteredBody(event.target.value);
-}
-function authorChangeHandler(event){
-  setEnteredAuthor(event.target.value);
-}
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
 
   return (
     <>
-     <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) :false}
+
       <ul className={styles.posts}>
-        <Post author={enteredAuthor}body={enteredBody} />
-        <Post author='Lorem' body='Lorem' />
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Lorem" body="Lorem" />
       </ul>
     </>
   );
